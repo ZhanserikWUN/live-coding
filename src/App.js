bd097d05bec54dd7ec5178df99df10eac9b9a6a3
Zhanserik <mpbszhan@gmail.com>
2023-05-12 20:27:29 +0600

    fix1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div
+        className="App"
+        style={{ display: 'flex', justifyContent: 'space-between' }}
+      >
+        <h1 className="NameProgrammer">Kuli Zhanserik</h1>
+        <div
+          className="buttons"
+          style={{ justifyContent: 'flex-end', alignItems: 'flex-start' }}
         >
-          Learn React
-        </a>
-      </header>
+          <button style={{ marginRight: '32px' }}>About</button>
+          <button style={{ marginRight: '32px' }}>Experience</button>
+          <button style={{ marginRight: '32px' }}>Projects</button>
+          <button style={{ marginRight: '32px' }}> Contacts</button>
+        </div>
+      </div>
+      <div className="secondComponent">
+        <div>
+          <div
+            style={{
+              width: '1280px',
+              height: '144px',
+              fontFamily: 'Courier New',
+              fontStyle: 'normal',
+              fontWeight: 'normal',
+              fontSize: '30px',
+              lineHeight: '36px',
+            }}
+          >
+            <h1>Hi, I’m Zhanserik, I build things for the web.</h1>
+          </div>
+          <div
+            className="second_component_two"
+            style={{ backgroundColor: 'purple', padding: '20px' }}
+          >
+            <p style={{ color: 'white' }}>
+              I’m software developer and this is my portfolio.
+            </p>
+          </div>
+          <div
+            className="second_comp_button"
+            style={{
+              width: '200px',
+              height: '50px',
+              backgroundColor: 'purple',
+              color: 'white',
+              border: 'none',
+              borderRadius: '10px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: '16px',
+            }}
+          >
+            <button>Check out my CV</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
